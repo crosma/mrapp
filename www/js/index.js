6914d03402b7ed1fa791a4404c59c1273c2de3d3
Matthew Crossley <vbaspcppguy@gmail.com>
2015-11-18 10:49:09 -0800

    Stuff and things

--- www/js/index.js
+++ www/js/index.js
@@ -17,145 +17,213 @@
  * under the License.
  */
 var app = {
-	version: 20,
+	version: 0.21,
 
     // Application Constructor
     initialize: function() {
 		$.support.cors = true; //make sure jquery has cors on
 
-		$('#version').text(this.version);
+		$('#version').text(app.version);
 
-        this.bindEvents();
+        app.bindEvents();
     },
 
 
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('deviceready', app.onDeviceReady, false);
     },
 
 
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+		//initialize push
 		var push = PushNotification.init({
 			"android": {"senderID": "129589237475"},
 			//"ios": {"alert": "true", "badge": "true", "sound": "true"},
 			//"windows": {}
 		});
 
-		console.log('Trying to register');
+		push.on('registration', app.onPushRegistration);
+		push.on('notification', app.onPushNotification);
+		push.on('error', app.onPushError);
 
-		push.on('registration', function (data) {
-			// data.registrationId
-			console.log('data = ', data);
-			console.log('data.registrationId = ', data.registrationId);
+		//get elements
+		app.form_login = $('#login');
+		app.button_login = app.form_login.find('input[type=submit]');
+		app.form_logout = $('#logout');
+		app.button_logout = app.form_logout.find('input[type=submit]');
+		app.div_message = $('#message');
+		app.input_username = $('#username');
+		app.input_password = $('#password');
 
-			app.registrationId = data.registrationId;
+		console.log(app.form_login);
+		console.log(app.form_logout);
+		console.log(app.div_message);
+		console.log(app.input_username);
+		console.log(app.input_password);
 
-			if (localStorage.logged_in) {
-				app.initLoggedIn();
-			} else {
-				app.initLoggedOut();
-			}
-		});
+		//prefill login form
+		app.input_username.val(localStorage.username);
+		app.input_password.val(localStorage.password);
 
-		push.on('notification', function (data) {
-			// data.message,
-			// data.title,
-			// data.count,
-			// data.sound,
-			// data.image,
-			// data.additionalData
-			console.log('Push!: ', data);
-		});
+		//set events
+		app.form_login.on('submit', app.onLoginClick);
+		app.form_logout.on('submit', app.onLogoutClick);
 
-		push.on('error', function (e) {
-			// e.message
-			console.error('Push Error: ', e);
-		});
-
+		//stuff
+		app.div_message.hide();
 	},
 
+
+	setStatus: function(msg) {
+		app.div_message.removeClass('error');
+
+		if (msg) {
+			app.div_message.text(msg).show();
+		} else {
+			app.div_message.hide();
+		}
+	},
+
+
+	setError: function(msg) {
+		app.setStatus(msg);
+		app.div_message.addClass('error');
+	},
+
+
 	initLoggedOut: function() {
-		var $status = $('#status').hide();
-		var $error = $('#error').hide();
+		app.form_login.show();
+		app.form_logout.hide();
 
-		var $form = $('#login').show();
-		var $username = $('#username');
-		var $password = $('#password');
-
-		//prefill form
-		$username.val(localStorage.username);
-		$password.val(localStorage.password);
-
-		$form.on('submit', function () {
-			var username = $username.val();
-			var password = $password.val();
-
-			//store form data
-			localStorage.username = username;
-			localStorage.password = password;
-
-			//hide form and show a logging in message...
-			$form.hide();
-			$error.hide();
-			$status.text('Logging in...').show();
-
-			var jqxhr = $.ajax({
-					type: "POST",
-					url: "https://mafiareturns.com/login_app.php",
-					data: {
-						'device': JSON.stringify(device),
-						'registrationId': app.registrationId,
-						'username': username,
-						'password': password
-					},
-				})
-				.done(function (data) {
-					console.log("done", arguments);
-
-					$status.hide();
-
-					data = JSON.parse(data);
-
-					if (data.res == 'ok') {
-						localStorage.logged_in = true;
-						app.initLoggedIn();
-
-					} else if (data.res == 'error') {
-						$error.html(data.msg).show();
-						$form.show();
-
-						delete localStorage.logged_in;
-					}
-				})
-				.fail(function (jqXHR, textStatus, errorThrown) {
-					console.log("fail", arguments);
-					
-					$status.hide();
-					$error.text('Error logging in: ' + textStatus).show();
-					$form.show();
-				})
-				.always(function () {
-					console.log("finished", arguments);
-				});
-
-			return false;
-		});
+		app.setStatus(false);
 	},
 
 
 	initLoggedIn: function() {
-		var $status = $('#status').text('This device is logged in as ' + localStorage.username + '. (TODO: ADD LOGOUT BUTTON)').show();
-		var $error = $('#error').hide();
-		var $form = $('#login').hide();
-	}
+		app.setStatus('This device is logged in as ' + localStorage.username + '. Logout to disable receiving notifications on this device.');
+
+		app.form_login.hide();
+		app.form_logout.show();
+	},
+
+
+	onLoginClick: function(e) {
+		console.log('onLoginClick');
+
+		var username = app.input_username.val().trim();
+		var password = app.input_password.val();
+
+		//store form data
+		localStorage.username = username;
+		localStorage.password = password;
+
+		//hide form and show a logging in message...
+		app.form_login.hide();
+		app.setStatus("Logging in...");
+
+		var jqxhr = $.ajax({
+				type: "POST",
+				url: "https://mafiareturns.com/login_app.php",
+				data: {
+					'action': 'login',
+					'device': JSON.stringify(device),
+					'registrationId': app.registrationId,
+					'username': username,
+					'password': password
+				},
+			})
+			.done(function (data) {
+				console.log("login done", arguments);
+
+				if (data.res == 'ok') {
+					localStorage.logged_in = true;
+					localStorage.acct_id = data.acct_id;
+
+					app.setStatus(false);
+					app.initLoggedIn();
+
+				} else if (data.res == 'error') {
+					delete localStorage.logged_in;
+					delete localStorage.acct_id;
+
+					app.setError(data.msg);
+					app.form_login.show();
+				}
+			})
+			.fail(function (jqXHR, textStatus, errorThrown) {
+				console.log("login fail", arguments);
+				app.setError('Error logging in: ' + textStatus);
+				app.form_login.show();
+			});
+
+		return false;
+	},
+
+
+	onLogoutClick: function(e) {
+		console.log('onLogoutClick');
+
+		app.form_logout.show();
+		app.setStatus("Logging out...");
+
+		var jqxhr = $.ajax({
+				type: "POST",
+				url: "https://mafiareturns.com/login_app.php",
+				data: {
+					'action': 'logout',
+					'uuid': device.uuid,
+					'acct_id': localStorage.acct_id,
+				},
+			})
+			.done(function (data) {
+				console.log("logout done", arguments);
+
+				delete localStorage.logged_in;
+				delete localStorage.acct_id;
+				app.initLoggedOut();
+
+				app.setStatus('You have been logged out.');
+
+			}).fail(function (jqXHR, textStatus, errorThrown) {
+				console.log("logout fail", arguments);
+				app.form_logout.show();
+				app.setError('Error logging out: ' + textStatus);
+
+			});
+
+		return false;
+	},
+
+
+	onPushRegistration: function (data) {
+		console.log('data.registrationId = ', data.registrationId);
+
+		app.registrationId = data.registrationId;
+
+		if (localStorage.logged_in) {
+			app.initLoggedIn();
+		} else {
+			app.initLoggedOut();
+		}
+	},
+
+
+	onPushNotification: function (data) {
+		// data.message,
+		// data.title,
+		// data.count,
+		// data.sound,
+		// data.image,
+		// data.additionalData
+		console.log('Push!: ', data);
+	},
+
+
+	onPushError: function (e) {
+		console.error('Push Error: ', e);
+
+		app.setError('Could not register to receive push messages: ' + e.message);
+	},
 };
 
 
