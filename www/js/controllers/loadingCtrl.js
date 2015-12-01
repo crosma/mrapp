21a8bef3d6e689cccb3bc9ef710c6c36f7242a6a
Matthew Crossley <vbaspcppguy@gmail.com>
2015-12-01 13:44:22 -0800

    Version push.

--- www/js/controllers/loadingCtrl.js
+++ www/js/controllers/loadingCtrl.js
@@ -1,5 +1,9 @@
 angular.module('app.controllers').controller('loadingCtrl', function ($scope, $rootScope, $sce) {
 	console.log('loadingCtrl');
 
+	$scope.version = '...';
 
+	cordova.getAppVersion.getVersionNumber().then(function (version) {
+		$scope.version = version;
+	});
 });
